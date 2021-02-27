7c87e5151f1cbdf8d756583e027e86399e9343bb
ray530 <rd.diaz887@uniandes.edu.co>
2021-02-26 19:44:09 -0500

    5 ya mejor

--- derivadas.cpp
+++ derivadas.cpp
@@ -12,6 +12,14 @@ double derivative(const function<double(double)>& f, double x0, double h) {
   return (f(hi) - f(lo)) / h;
 }
 
+double Second_derivative(const function<double(double)>& f, double x0, double h) {
+  double h2 = h / 2;
+  double lo = x0 - h2;
+  double hi = x0 + h2;
+  return (f(hi) - f(lo)) / h;
+}
+
+
 double g(double x) {
   return x-sin(x);
 }
@@ -27,17 +35,20 @@ int main() {
     double app = derivative(g, x, h);
     double errlocal1 = ans - app;
     if (errlocal1<0) { err1 = -errlocal1; }
-    cout << "With h ==" << h << " estimate f'(" << x << ") is " << app << " error local ==" << err1 << endl;
+    double errglobal1;
+    errglobal1 = errlocal1/ans;
+    cout << "With h ==" << h << " estimate f'(" << x << ") is " << app << " error local ==" << err1 << " error global =="<< errglobal1 << endl;
     n = n+1;  
   }
   
   double x_inter [] = {-1,  -0.95, 	-0.9,	-0.85,	-0.8,	-0.75,	-0.700000000000001,	-0.650000000000001,	-0.600000000000001,	-0.550000000000001,	-0.500000000000001,	-0.450000000000001,	-0.400000000000001,	-0.350000000000001,	-0.300000000000001,	-0.250000000000002,	-0.200000000000002,	-0.150000000000002,	-0.100000000000002,	-0.050000000000002,	-1.99840144432528E-15, 0.049999999999998,	0.099999999999998,	0.149999999999998,	0.199999999999998,	0.249999999999998,	0.299999999999997,	0.349999999999997,	0.399999999999997,	0.449999999999997,	0.499999999999997,	0.549999999999997,	0.599999999999997,	0.649999999999997,	0.699999999999997,	0.749999999999996,	0.799999999999996,	0.849999999999996,	0.899999999999996,	0.949999999999996,	1};
   
   double err2 = 0;
+  double errglobal =0;
   
   ofstream archivo;
   archivo.open("datosderivada.txt", ios::out);
-  
+
   
   for (int i=0; i<=40; i++)
   {
@@ -47,9 +58,17 @@ int main() {
   	double dy = derivative(g, x_inter[i], h);
 	double errlocal2 = y_prima - dy;
 	if (errlocal2<0) { err2 = -errlocal2; }
-  	cout << x_inter[i]<< " " << y << " " << dy << " " << y_prima << " " << err2 << endl;
-  	archivo << x_inter[i]<< " " << y << " " << dy << " " << y_prima << " " << err2 << endl;
+  	
+  	double suma_analitica = 0;
+  	double suma_err = 0;
+  	suma_err = suma_err +  pow(err2,2);
+  	suma_analitica = suma_analitica + pow(y_prima,2);
+  	
+  	double errglobal = pow((suma_err/suma_analitica),0.5);
+  	cout << x_inter[i]<< " " << y << " " << dy << " " << y_prima << " " << err2 << " " << errglobal << endl;
+  	archivo << x_inter[i]<< " " << y << " " << dy << " " << y_prima << " " << err2 << " " << errglobal << endl;
   }
+  	
   
   archivo.close();
   return 0;
--- graficaderivadas.py
+++ graficaderivadas.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 
 data=np.loadtxt('datosderivada.txt')
 
-plt.figure()
-plt.subplot(2,1,1)
+plt.figure(figsize=(12,6))
+plt.subplot(1,3,1)
 plt.scatter(data[:,0],data[:,1],label="F(x)=x-sin(x)", color="blue")
 plt.scatter(data[:,0],data[:,3],label="F'(x) númerico", color="black")
 plt.plot(data[:,0],data[:,2],label="F'(x) análitico", color="red")
 plt.xlabel("X")
 plt.legend()
-plt.subplot(2,1,2)
-plt.plot(data[:,0],data[:,-1],label="Error Local", color="green")
+plt.subplot(1,3,2)
+plt.plot(data[:,0],data[:,-2],label="Error Local", color="green")
+plt.xlabel("X")
+plt.legend()
+plt.subplot(1,3,3)
+plt.plot(data[:,0],data[:,-1],label="Error global", color="black")
 plt.xlabel("X")
 plt.legend()
 plt.savefig("Grafica_derivada_errorlocal.png")
 plt.show()
+
 
 
